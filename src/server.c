1d8467502372cb8a24ef2a48c461809436bc4101
dmatavel <matavelidanilo@gmail.com>
2022-12-12 18:01:35 -0300

    Pause to wait for a signal

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 15:29:26 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/12/12 15:48:24 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/12/12 17:40:33 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@ int	main(void)
 	
 	process_id = getpid();
 	ft_printf("%d\n", process_id);
+	pause();
 	return (0);
 }
