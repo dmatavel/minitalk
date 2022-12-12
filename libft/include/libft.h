14707d79894603b5485b6ded54981c6b82babb83
dmatavel <matavelidanilo@gmail.com>
2022-12-12 15:11:19 -0300

    Add libft

---
+++ libft/include/libft.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmatavel <dmatavel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/06 13:27:04 by dmatavel          #+#    #+#             */
+/*   Updated: 2022/09/21 16:51:10 by dmatavel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+/* standard libc remakes */
+# include "libft_ctype.h"
+# include "libft_string.h"
+# include "libft_strings.h"
+# include "libft_stdlib.h"
+# include "libft_stdio.h"
+
+/* nonstandard libc remakes */
+# include "libft_bsd.h"
+
+/* lbft extensions*/
+# include "libft_str.h"
+# include "libft_extends.h"
+# include "libft_tools.h"
+
+#endif
