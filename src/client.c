c17b05753bd3937724d6e97f9557c9d7b985d5e6
dmatavel <matavelidanilo@gmail.com>
2022-12-14 10:20:14 -0300

    Uses ft_atoi to get the PID from server and send SIGUSR1 via kill()

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 17:36:07 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/12/12 17:57:20 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/12/14 10:14:55 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,15 @@
 
 int	main(int argc, char *argv[])
 {
+	int	server_pid;
+	int r = 0;
+	
+	server_pid = ft_atoi(argv[1]);
+	ft_printf("server pid: %d\n", server_pid);
 	if (argc == 2)
-		kill(argv[1][1], 0);
-	ft_printf("%s", argv[1]);
+	{
+		r = kill(server_pid, SIGUSR1);
+		ft_printf("kill return: %d\n", r);
+	}
 	return (0);
 }
