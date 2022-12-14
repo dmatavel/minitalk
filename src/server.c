a7d08d88c9c9180e44d2a1d1d498293b942135c1
dmatavel <matavelidanilo@gmail.com>
2022-12-14 10:19:22 -0300

    Add signal() and signal_handler

--- src/server.c
+++ src/server.c
@@ -6,18 +6,29 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 15:29:26 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/12/12 17:40:33 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/12/14 10:01:04 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/include/libft.h"
 
+void	handler(int signum);
+
 int	main(void)
 {
 	pid_t	process_id;
 	
+	signal(SIGUSR1, handler);
 	process_id = getpid();
 	ft_printf("%d\n", process_id);
-	pause();
+	while (1)
+		sleep(1);
 	return (0);
 }
+
+void	handler(int signum)
+{
+	if (signum == SIGUSR1)
+		write(STDOUT_FILENO, "Hello, world\n", 13);
+}
+
