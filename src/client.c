279061027e57f4ea4a2905536d020800b88f7127
dmatavel <matavelidanilo@gmail.com>
2022-12-19 22:22:07 -0300

    Change include path

--- src/client.c
+++ src/client.c
@@ -6,11 +6,11 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 17:36:07 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/12/14 14:07:45 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/12/19 22:18:04 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/include/libft.h"
+#include "../include/libft/include/libft.h"
 
 void	messenger(char *msg, int serv_pid);
 void	bits(int n, int serv_pid);
@@ -19,20 +19,16 @@ int	main(int argc, char *argv[])
 {
 	int	serv_pid;
 	char	*msg;
-	int i;
-	int	msg_len;
 	if (argc < 2)
 	{
 		ft_printf("Please, insert Server's PID and a valid message.");
 		return (1);
 	}	
-	i = 0;
 	serv_pid = ft_atoi(argv[1]);
 	msg = malloc(sizeof(char) * (ft_strlen(argv[2]) + 1));
 	if (!msg)
 		return (0);
 	ft_strcpy(msg, argv[2]);
-	msg_len = ft_strlen(msg);
 	messenger(msg, serv_pid);
 	return (0);
 }
